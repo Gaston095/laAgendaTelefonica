f604b6f31b34e8bf65a47964320ad0469ad0c5e1
Gaston095 <Gastonsalas095@gmail.com>
2024-08-06 22:22:40 -0300

    Se implementa un campo de busqueda para filtrar la lista por nombres

--- agenda-telefonica/src/App.jsx
+++ agenda-telefonica/src/App.jsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-     }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     
     const personOBject = {
       name : newName,
-      number: newNumber
+      number: newNumber,
+      id : persons.length + 1
     }
 
     const includesRepeatName = persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase())
@@ -38,14 +40,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-/*   const includesDuplicatesNames = () => {
-    const arrayNames = persons.map(person => person.name)
-    (arrayNames.includes(newName))
-  } */
+  const handleFilterNameChange = (event) => {
+    setFilterName(event.target.value)
+  }
+
+  const personsToShow = (filterName === '')
+  ? persons
+  : persons.filter(person => person.name.toLowerCase() === filterName.toLowerCase())
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <label>filter shown with</label><input type='text' value={filterName} onChange={handleFilterNameChange}/>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange}/><br />
@@ -57,7 +64,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {personsToShow.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
